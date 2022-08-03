39187e17adb1a1fc6d202af075d7593de069cc95
jeptun <jeptun@gmail.com>
2022-08-03 20:36:09 +0200

    Create project

---
+++ app/js/script.js
@@ -0,0 +1,13 @@
+const testString = 'Javascript test!';
+
+console.log(testString);
+
+const fruits = [
+  '😀',
+  '✨',
+  '💀',
+];
+
+fruits.map((fruit) => {
+  console.log(fruit);
+});
---
+++ dist/script.js
@@ -0,0 +1,2 @@
+const testString="Javascript test!";console.log(testString);const fruits=["😀","✨","💀"];fruits.map((t=>{console.log(t)}));
+//# sourceMappingURL=script.js.map
